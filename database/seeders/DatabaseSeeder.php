d16ef3dfb1518c4dc0f4fa994b1cfb6c4e2d08b6
AnhMH <anh.mh@moziasoft.com>
2020-10-11 09:55:55 +0700

    Add authenticate

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // User::factory(10)->create();
+        DB::table('users')->insert([
+            'name' => Str::random(10),
+            'email' => 'anhmh@gmail.com',
+            'password' => Hash::make('123456789'),
+        ]);
     }
 }
